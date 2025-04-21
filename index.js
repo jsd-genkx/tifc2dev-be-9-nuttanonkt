629a6c76b683cd113848cdab4bee8a4a373ca14e
nuttanon.kt <nuttanon321@gmail.com>
2025-04-21 15:35:56 +0700

    First commit

--- index.js
+++ index.js
@@ -19,28 +19,55 @@ const books = [
 app.get("/books", (req, res, next) => {
   setTimeout(() => {
     const { genre } = req.query;
-    //TODO: ADD CODE HERE ⬇️ to Filter books by genre.
-    const filteredBooks = books.filter((book) => book.genre.includes(genre));
-    //TODO: ADD CODE HERE ⬇️
+    //TODO: ADD CODE to Filter books by genre.
+    try{
+    if (genre) {
+      const filteredBooks = books.filter((book) => book.genre.includes(genre));
+      res.send(filteredBooks);
+    } else {
+      res.send(books);
+    }
+  }catch(error){
+    next(error);
+  }
   }, 1000); // Simulate a 1-second delay
 });
 
 // GET specific book by ID with async/await
 app.get("/books/:id", async (req, res, next) => {
-  const book = await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const foundBook = books.find((b) => b.id === parseInt(req.params.id, 10));
-      if (foundBook) {
-        resolve(foundBook);
-      } else {
-        //TODO: ADD CODE to reject the promise
-      }
-    }, 1000); // Simulate a 1-second delay
-  });
-  //TODO: ADD CODE HERE ⬇️
+  try {
+    const book = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const foundBook = books.find(
+          (b) => b.id === parseInt(req.params.id, 10)
+        );
+        if (foundBook) {
+          resolve(foundBook);
+        } else {
+          //TODO: ADD CODE to reject the promise
+          reject(new Error("Not found"));
+        }
+      }, 1000); // Simulate a 1-second delay
+    });
+    //TODO: ADD CODE
+    res.send(book);
+  } catch (error) {
+    error.status = 404;
+    next(error);
+  }
 });
 
-//TODO: ADD CODE HERE ⬇️
+//TODO: ADD CODE
+app.use((err, req, res, next) => {
+  const status = err.status;
+  const response = {
+    message: err.message,
+    status: status,
+  };
+
+  console.error(err.stack);
+  res.status(status).json(response);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
